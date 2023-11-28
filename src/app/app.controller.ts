fc5d1922c3f91963e5c02c8a0c78c882544ae6e3
cesousan <cesar0sabater@gmail.com>
2023-11-28 14:26:14 +0100

    feat(webinar): controller payload validation

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,8 +1,11 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+
 import { User } from '../entities';
+import { ZodValidationPipe } from '../pipes/zod-validation.pipe';
 import { OrganizeWebinar } from '../usecases/organize-webinar';
+
 import { AppService } from './app.service';
-import { addDays, addHours, parseISO } from 'date-fns';
+import { WebinarAPI } from './contract';
 
 @Controller()
 export class AppController {
@@ -16,13 +19,16 @@ export class AppController {
     return this.appService.getHello();
   }
   @Post('/webinars')
-  async handleOrganizeWebinar(@Body() body: any) {
+  async handleOrganizeWebinar(
+    @Body(new ZodValidationPipe(WebinarAPI.OrganizeWebinar.schema))
+    body: WebinarAPI.OrganizeWebinar.Request,
+  ): Promise<WebinarAPI.OrganizeWebinar.Response> {
     return this.organizeWebinar.execute({
       user: new User({ id: 'john-doe' }),
       title: body.title,
       seats: body.seats,
-      startDate: new Date(body.startDate),
-      endDate: new Date(body.endDate),
+      startDate: body.startDate,
+      endDate: body.endDate,
     });
   }
 }
---
+++ src/app/contract.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+export namespace WebinarAPI {
+  export namespace OrganizeWebinar {
+    export const schema = z.object({
+      title: z.string(),
+      seats: z.number(),
+      startDate: z.coerce.date(),
+      endDate: z.coerce.date(),
+    });
+    export type Request = z.infer<typeof schema>;
+    export type Response = { id: string };
+  }
+}
